cbb2820568feaf0eee98bc41c0ee932c74ea2a10
Ernesto Melian <you@example.com>
2024-02-13 23:30:36 -0300

    updated api access

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -1,5 +1,5 @@
 const API = "https://api.themoviedb.org/3";
-const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGE5ZGI0ZDA0NGRjNzc1ZmZkNmFiMWFjYjc4YzQyMSIsInN1YiI6IjYwNzcxNDBmMmZhZjRkMDA3OGE5OTljZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Co9fPSiZBXSndLwPeG3fCaVywTmgc-YyhQ_hQ2hS9ps";
+const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGU4Yjk5YzYxZTg4Nzg2ZDc3NDgzYjA4OTg3Y2Y4MiIsInN1YiI6IjYwNzcxNDBmMmZhZjRkMDA3OGE5OTljZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BbSsJN8B-OcX4uZb9Hgs4p5SK6B_uAo3r00DbBc7Zl4";
 export function get(path) {
     return fetch( API + path, {
       headers: {
