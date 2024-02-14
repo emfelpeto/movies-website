d9938519065ae9e9e001a07ca48927e7bfbf5a15
Ernesto Melian <you@example.com>
2024-02-13 23:38:04 -0300

    deleted var not used

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 import { get } from "../utils/httpClient";
 
 export function MoviesGrid() {
-  const access_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNGU4Yjk5YzYxZTg4Nzg2ZDc3NDgzYjA4OTg3Y2Y4MiIsInN1YiI6IjYwNzcxNDBmMmZhZjRkMDA3OGE5OTljZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BbSsJN8B-OcX4uZb9Hgs4p5SK6B_uAo3r00DbBc7Zl4";
   const [movies, setMovies] = useState([]);
   useEffect(() => {
     get("/discover/movie").then(data=>{
